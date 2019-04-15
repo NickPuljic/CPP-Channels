9073826c9e1e3a5d0065626fb8ab4d51a6b433d9
Nick Puljic <nap2152@columbia.edu>
2019-04-15 18:34:58 -0400

    cleaned up tests

--- src/channelsTest.cpp
+++ src/channelsTest.cpp
@@ -41,36 +41,41 @@ void must_stay_blocked(Chan<int>& chan) {
     REQUIRE(1 == 2);
 }
 
-TEST_CASE( "non-blocking send and recive test" ) {
+TEST_CASE("nonblocking send and recive test") {
     Chan<int> c1;
 
-    SECTION( "test nonblocking send" ) {
+    SECTION("test nonblocking send") {
+        // Send should fail
         REQUIRE(c1.send_nonblocking(5) == false);
 
+        // Recv 10 and block then wait a second
         std::thread t1{recv_n, std::ref(c1), 10};
         std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        // Send 10 nonblocking and make sure it succeeds
         REQUIRE(c1.send_nonblocking(10) == true);
         t1.join();
     }
-
-    SECTION( "test nonblocking recv" ) {
+    SECTION("test nonblocking recv") {
         int r = 0;
+        // Make sure you can't recv nonblocking
         REQUIRE(c1.recv_nonblocking(r) == false);
 
+        // Send 10 then wait
         std::thread t1{send_n, std::ref(c1), 10};
         std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        // Make sure you can now recv and get correct value
         REQUIRE(c1.recv_nonblocking(r) == true);
         REQUIRE(r == 10);
         t1.join();
     }
-
 }
 
-
-TEST_CASE( "copy and move constructors" ) {
+TEST_CASE("copy and move constructors") {
     Chan<int> c1(5);
 
-    SECTION ( "test move constructor" ) {
+    SECTION("test move constructor") {
         // Send c1 int 5 and 7
         std::thread t1{send_n, std::ref(c1), 5};
         t1.join();
@@ -88,12 +93,11 @@ TEST_CASE( "copy and move constructors" ) {
 
         // Check if 5 is still in c1 (it shouldnt be)
         std::thread t5{recv_n, std::ref(c1), -1};
-        // Give t5 1 second to execute
+        // Give t5 1 second to execute then detatch
         std::this_thread::sleep_for(std::chrono::seconds(1));
         t5.detach();
     }
-
-    SECTION ( "test copy constructor" ) {
+    SECTION("test copy constructor") {
         // Send c1 int 5 and 7
         std::thread t1{send_n, std::ref(c1), 5};
         t1.join();
@@ -114,32 +118,34 @@ TEST_CASE( "copy and move constructors" ) {
         t5.join();
         std::thread t6{recv_n, std::ref(c1), 7};
         t6.join();
-
     }
 }
 
-
-
-TEST_CASE( "unbuffered channel" ) {
+TEST_CASE("unbuffered channel") {
     Chan<int> chan;
 
-    SECTION( "unbuffered blocking two channels") {
+    SECTION("unbuffered blocking two channels") {
+        // Send value to unbuffered channel and block
         std::thread t1{send_n_to_channel, std::ref(chan), 1};
         std::this_thread::sleep_for(std::chrono::seconds(1));
+        // Should fail if unbuffered channel recv a second time
         std::thread t2{must_stay_blocked, std::ref(chan)};
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
+        // Recv from first block and make sure its correct
         int i = chan.recv();
         REQUIRE(i == 0);
         t1.join();
         t2.detach();
     }
-    SECTION( "unbuffered receive twice") {
+    SECTION("unbuffered receive twice") {
+        // Send on two channels
         std::thread t1{send_n, std::ref(chan), 5};
         std::this_thread::sleep_for(std::chrono::seconds(1));
         std::thread t2{send_n, std::ref(chan), 7};
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
+        // Unblock the sends and verify correct
         REQUIRE(chan.recv() == 5);
         REQUIRE(chan.recv() == 7);
 
@@ -148,10 +154,10 @@ TEST_CASE( "unbuffered channel" ) {
     }
 }
 
-TEST_CASE( "sending and receiving" ) {
+TEST_CASE("sending and receiving") {
     Chan<int> chan = Chan<int>(15);
 
-    SECTION( "sending and receiving async" ) {
+    SECTION("sending and receiving async") {
         std::thread t1{send_n_to_channel, std::ref(chan), 15};
 
         // Wait for them all to send
@@ -161,7 +167,7 @@ TEST_CASE( "sending and receiving" ) {
 
         t2.join();
     }
-    SECTION( "recv assignment") {
+    SECTION("recv assignment") {
         std::thread t1{send_n_to_channel, std::ref(chan), 15};
 
         // Wait for them all to send
@@ -171,7 +177,7 @@ TEST_CASE( "sending and receiving" ) {
 
         t2.join();
     }
-    SECTION( "receiving first sync" ) {
+    SECTION("receiving first sync") {
         std::thread t1{recv_n_from_channel, std::ref(chan), 15};
 
         // Give the recv a second to make sure it is waiting
@@ -182,7 +188,7 @@ TEST_CASE( "sending and receiving" ) {
         t2.join();
         t1.join();
     }
-    SECTION( "receiving twice first sync" ) {
+    SECTION("receiving twice first sync") {
         std::thread t1{recv_n, std::ref(chan), 0};
         // Give the rec a second to wait
         std::this_thread::sleep_for(std::chrono::seconds(1));
@@ -196,7 +202,7 @@ TEST_CASE( "sending and receiving" ) {
         t1.join();
         t2.join();
     }
-    SECTION( "sending first sync" ) {
+    SECTION("sending first sync") {
         std::thread t1{send_n_to_channel, std::ref(chan), 15};
         std::thread t2{recv_n_from_channel, std::ref(chan), 15};
 
