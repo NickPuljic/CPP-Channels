89f7df134978ed3450d40b807c736dda6b4120d9
Jacob Gold <goldjacobe@gmail.com>
2019-04-19 11:36:44 -0400

    add template keyword to buffer impl

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -1,29 +1,39 @@
 #include "buffer.h"
 
-explicit Buffer::Buffer(size_t n) {
+template<typename T>
+explicit Buffer<T>::Buffer(size_t n) {
     cap = n;
 }
 
-Buffer::Buffer(const Buffer &b) :
+template<typename T>
+Buffer<T>::Buffer(const Buffer &b) :
     q(b.q),
     cap(b.cap),
     cur_size() {
         cur_size = b.cur_size.load();
     }
 
-Buffer::Buffer(Buffer &&b) :
+template<typename T>
+Buffer<T>::Buffer(Buffer &&b) :
     q(std::move(b.q)),
     cap(std::move(b.cap)),
     cur_size() {
         cur_size = b.cur_size.exchange(0);
     }
 
-void Buffer::push(const T& elem) {q.push(elem); cur_size++;}
-void Buffer::push(T&& elem) {q.push(elem); cur_size++;}
+template<typename T>
+void Buffer<T>::push(const T& elem) {q.push(elem); cur_size++;}
+template<typename T>
+void Buffer<T>::push(T&& elem) {q.push(elem); cur_size++;}
 
-T& Buffer::front() {return q.front();}
-void Buffer::pop() {q.pop(); cur_size--;}
+template<typename T>
+T& Buffer<T>::front() {return q.front();}
+template<typename T>
+void Buffer<T>::pop() {q.pop(); cur_size--;}
 
-size_t Buffer::current_size() {return cur_size.load();}
-size_t Buffer::capacity() {return cap;}
-bool Buffer::is_full() {return cap == cur_size.load();}
+template<typename T>
+size_t Buffer<T>::current_size() {return cur_size.load();}
+template<typename T>
+size_t Buffer<T>::capacity() {return cap;}
+template<typename T>
+bool Buffer<T>::is_full() {return cap == cur_size.load();}
