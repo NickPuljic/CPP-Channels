64e5a104940bb4f08b7f339c569a35e57be8d397
Nick Puljic <nap2152@columbia.edu>
2019-04-30 18:50:08 -0400

    Removed std::ref() around channels

--- src/channelsTest.cpp
+++ src/channelsTest.cpp
@@ -2,13 +2,13 @@
 #include "libs/catch.hpp"
 #include "chan.h"
 
-void send_n_to_channel(Chan<int>& chan, int n) {
+void send_n_to_channel(Chan<int> chan, int n) {
     for (int i = 0; i < n; i++) {
         chan.send(i);
     }
 }
 
-void recv_n_from_channel(Chan<int>& chan, int n) {
+void recv_n_from_channel(Chan<int> chan, int n) {
     int num;
     for (int i = 0; i < n; i++) {
         chan.recv(num);
@@ -16,7 +16,7 @@ void recv_n_from_channel(Chan<int>& chan, int n) {
     }
 }
 
-void recv_assignment_n_from_channel(Chan<int>& chan, int n) {
+void recv_assignment_n_from_channel(Chan<int> chan, int n) {
     int num;
     for (int i = 0; i < n; i++) {
         num = chan.recv();
@@ -24,28 +24,28 @@ void recv_assignment_n_from_channel(Chan<int>& chan, int n) {
     }
 }
 
-void send_n(Chan<int>& chan, int n) {
+void send_n(Chan<int> chan, int n) {
     chan.send(n);
 }
 
-void recv_n(Chan<int>& chan, int n) {
+void recv_n(Chan<int> chan, int n) {
     int num;
     chan.recv(num);
     REQUIRE(num == n);
 }
 
-void must_stay_blocked(Chan<int>& chan) {
+void must_stay_blocked(Chan<int> chan) {
     chan.send(-1);
     // This line can never execute
     REQUIRE(1 == 2);
 }
 
-void send_n_and_close(Chan<int>& chan, int n) {
+void send_n_and_close(Chan<int> chan, int n) {
     send_n_to_channel(chan, n);
     chan.close();
 }
 
-void recv_n_using_foreach(Chan<int>& chan) {
+void recv_n_using_foreach(Chan<int> chan) {
     int i = 0;
     chan.foreach([=](int num) mutable {
         REQUIRE(num == i);
@@ -53,13 +53,13 @@ void recv_n_using_foreach(Chan<int>& chan) {
     });
 }
 
-void send_all(Chan<int>& chan, std::vector<int>& v) {
+void send_all(Chan<int> chan, std::vector<int>& v) {
     for (auto num : v) {
         chan.send(num);
     }
 }
 
-void recv_for_seconds(Chan<int>& chan, std::vector<int>& recver_data, unsigned& seconds) {
+void recv_for_seconds(Chan<int> chan, std::vector<int>& recver_data, unsigned& seconds) {
 
     auto start = std::chrono::high_resolution_clock::now();
 
@@ -110,13 +110,13 @@ void parallel_send_and_recv(
     // each recveiver will recv everything it can for recv_for seconds.
     // assumes all sent data will be recved during the given seconds.
     for (auto i = 0; i < n_recvers; ++i) {
-        std::thread t{recv_for_seconds, std::ref(chan), std::ref(each_recver_data[i]), std::ref(recv_for)};
+        std::thread t{recv_for_seconds, chan, std::ref(each_recver_data[i]), std::ref(recv_for)};
         t.detach();
     }
 
     // launch all senders.
     for (auto i = 0; i < n_senders; ++i){
-        std::thread t{send_all, std::ref(chan), std::ref(each_sender_data[i])};
+        std::thread t{send_all, chan, std::ref(each_sender_data[i])};
         t.detach();
     }
 
@@ -147,7 +147,7 @@ TEST_CASE("nonblocking send and recive test") {
         REQUIRE(c1.send_nonblocking(5) == false);
 
         // Recv 10 and block then wait a second
-        std::thread t1{recv_n, std::ref(c1), 10};
+        std::thread t1{recv_n, c1, 10};
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
         // Send 10 nonblocking and make sure it succeeds
@@ -160,7 +160,7 @@ TEST_CASE("nonblocking send and recive test") {
         REQUIRE(c1.recv_nonblocking(r) == false);
 
         // Send 10 then wait
-        std::thread t1{send_n, std::ref(c1), 10};
+        std::thread t1{send_n, c1, 10};
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
         // Make sure you can now recv and get correct value
@@ -176,46 +176,46 @@ TEST_CASE("copy and move constructors") {
 
     SECTION("test move constructor") {
         // Send c1 int 5 and 7
-        std::thread t1{send_n, std::ref(c1), 5};
+        std::thread t1{send_n, c1, 5};
         t1.join();
-        std::thread t2{send_n, std::ref(c1), 7};
+        std::thread t2{send_n, c1, 7};
         t2.join();
 
         // Make a move of c1 to c2
         Chan<int> c2 = std::move(c1);
 
         // Check that c2 now has 5 and 7
-        std::thread t3{recv_n, std::ref(c2), 5};
+        std::thread t3{recv_n, c2, 5};
         t3.join();
-        std::thread t4{recv_n, std::ref(c2), 7};
+        std::thread t4{recv_n, c2, 7};
         t4.join();
 
         // Check if 5 is still in c1 (it shouldnt be)
-        //std::thread t5{recv_n, std::ref(c1), -1};
+        //std::thread t5{recv_n, c1, -1};
         // Give t5 1 second to execute then detatch
         //std::this_thread::sleep_for(std::chrono::seconds(1));
         //t5.detach();
     }
     SECTION("test copy constructor") {
         // Send c1 int 5 and 7
-        std::thread t1{send_n, std::ref(c1), 5};
+        std::thread t1{send_n, c1, 5};
         t1.join();
-        std::thread t2{send_n, std::ref(c1), 7};
+        std::thread t2{send_n, c1, 7};
         t2.join();
 
         // Make a copy of c1 to c2
         Chan<int> c2 = c1;
 
         // Check that c2 now has 5 and 7
-        std::thread t3{recv_n, std::ref(c2), 5};
+        std::thread t3{recv_n, c2, 5};
         t3.join();
-        std::thread t4{recv_n, std::ref(c2), 7};
+        std::thread t4{recv_n, c2, 7};
         t4.join();
 
         // Check that c1 also has 5 and 7
-        //std::thread t5{recv_n, std::ref(c1), 5};
+        //std::thread t5{recv_n, c1, 5};
         //t5.join();
-        //std::thread t6{recv_n, std::ref(c1), 7};
+        //std::thread t6{recv_n, c1, 7};
         //t6.join();
     }
 }
@@ -224,11 +224,11 @@ TEST_CASE("unbuffered channel") {
     SECTION("unbuffered blocking two channels") {
         Chan<int> chan;
         // Send value to unbuffered channel and block
-        std::thread t1{send_n, std::ref(chan), 8};
+        std::thread t1{send_n, chan, 8};
         std::this_thread::sleep_for(std::chrono::seconds(1));
         // throws exception because ChanData destructor is not yet implemented.
         // Should fail if unbuffered channel recv a second time
-        // std::thread t2{must_stay_blocked, std::ref(chan)};
+        // std::thread t2{must_stay_blocked, chan};
         // std::this_thread::sleep_for(std::chrono::seconds(1));
 
         // Recv from first block and make sure its correct
@@ -240,9 +240,9 @@ TEST_CASE("unbuffered channel") {
     SECTION("unbuffered receive twice") {
         Chan<int> chan;
         // Send on two channels
-        std::thread t1{send_n, std::ref(chan), 5};
+        std::thread t1{send_n, chan, 5};
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::thread t2{send_n, std::ref(chan), 7};
+        std::thread t2{send_n, chan, 7};
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
         // Unblock the sends and verify correct
@@ -258,54 +258,54 @@ TEST_CASE("sending and receiving") {
     Chan<int> chan = Chan<int>(150);
 
     SECTION("sending and receiving async") {
-        std::thread t1{send_n_to_channel, std::ref(chan), 150};
+        std::thread t1{send_n_to_channel, chan, 150};
 
         // Wait for them all to send
         t1.join();
 
-        std::thread t2{recv_n_from_channel, std::ref(chan), 150};
+        std::thread t2{recv_n_from_channel, chan, 150};
 
         t2.join();
     }
     SECTION("recv assignment") {
-        std::thread t1{send_n_to_channel, std::ref(chan), 150};
+        std::thread t1{send_n_to_channel, chan, 150};
 
         // Wait for them all to send
         t1.join();
 
-        std::thread t2{recv_assignment_n_from_channel, std::ref(chan), 150};
+        std::thread t2{recv_assignment_n_from_channel, chan, 150};
 
         t2.join();
     }
     SECTION("receiving first sync") {
-        std::thread t1{recv_n_from_channel, std::ref(chan), 150};
+        std::thread t1{recv_n_from_channel, chan, 150};
 
         // Give the recv a second to make sure it is waiting
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
-        std::thread t2{send_n_to_channel, std::ref(chan), 150};
+        std::thread t2{send_n_to_channel, chan, 150};
 
         t2.join();
         t1.join();
     }
     SECTION("receiving twice first sync") {
-        std::thread t1{recv_n, std::ref(chan), 0};
+        std::thread t1{recv_n, chan, 0};
         // Give the rec a second
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
-        std::thread t2{recv_n, std::ref(chan), 1};
+        std::thread t2{recv_n, chan, 1};
         std::this_thread::sleep_for(std::chrono::seconds(1));
 
-        std::thread t3{send_n_to_channel, std::ref(chan), 2};
+        std::thread t3{send_n_to_channel, chan, 2};
 
         t3.join();
         t1.join();
         t2.join();
     }
     SECTION("sending first sync") {
-        std::thread t1{send_n_to_channel, std::ref(chan), 150};
+        std::thread t1{send_n_to_channel, chan, 150};
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::thread t2{recv_n_from_channel, std::ref(chan), 150};
+        std::thread t2{recv_n_from_channel, chan, 150};
 
         t1.join();
         t2.join();
@@ -317,16 +317,16 @@ TEST_CASE( "send, close, and recv using for range" ) {
 
     SECTION("send and close then range") {
         // Send 0, 1, 2 to chan and close
-        std::thread t1{send_n_and_close, std::ref(chan), 200};
+        std::thread t1{send_n_and_close, chan, 200};
         t1.join();
 
         // foreach through channel and make sure it can recv
-        std::thread t2{recv_n_using_foreach, std::ref(chan)};
+        std::thread t2{recv_n_using_foreach, chan};
         t2.join();
     }
     SECTION("foreach should have nothing with empty channel") {
         chan.close();
-        std::thread t1{recv_n_using_foreach, std::ref(chan)};
+        std::thread t1{recv_n_using_foreach, chan};
         t1.join();
     }
 }
@@ -335,7 +335,7 @@ TEST_CASE("test close") {
     Chan<int> chan = Chan<int>(150);
 
     SECTION("close after send") {
-        std::thread t1{send_n_to_channel, std::ref(chan), 150};
+        std::thread t1{send_n_to_channel, chan, 150};
         t1.join();
         chan.close();
         recv_n_from_channel(chan, 150);
@@ -345,4 +345,3 @@ TEST_CASE("test close") {
 TEST_CASE( "parallel send and recv" ) {
     parallel_send_and_recv();
 }
-
